f742b8704f9efec92f98e68a43ecbc505fe0a3b3
Myname <3224137526@qq.com>
2023-04-14 15:44:12 +0800

    空项目

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    "plugin:vue/essential",
+    "eslint:recommended",
+    //空白换行报错
+    // "plugin:prettier/recommended"
+  ],
+  parserOptions: {
+    parser: "@babel/eslint-parser",
+    // template有蓝色波浪线
+    requireConfigFile: false,
+  },
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+  },
+};
